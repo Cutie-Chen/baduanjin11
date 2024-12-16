248d791e4ead2cf390d9cdf262d7cbd5e175dd6f
Cutie-Chen <2418707510@qq.com>
2024-12-16 17:39:36 +0800

    世界构建

---
+++ Assets/scripts/text.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class text : MonoBehaviour
+{
+     public TextMeshProUGUI t;
+     
+    // Start is called before the first frame update
+    void Start()
+    {
+        t.text="http://47.103.52.112:8866/bili/api?id=BV1gT4y1m7ec";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
