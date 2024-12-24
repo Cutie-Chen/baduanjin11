948d0f6a92b8df8e82a2ffd7814e38187f52f3d8
Cutie-Chen <2418707510@qq.com>
2024-12-24 21:57:19 +0800

    动画完成

---
+++ Assets/scripts/Anim.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Anim : MonoBehaviour
+{
+    public Animator mainFlowerAnimator;  // 主花的 Animator，必须是 public，用于从 Inspector 绑定
+    private Animator flowerAnimator;      // 当前花的 Animator，不需要是 public，直接获取
+
+    void Start()
+    {
+        // 获取 Animator 组件
+        flowerAnimator = GetComponent<Animator>();    }
+
+    void Update()
+    {
+         if (mainFlowerAnimator.GetCurrentAnimatorStateInfo(0).IsName("open"))
+        {
+            // 主花的动画触发了，立即触发其他花的动画
+            flowerAnimator.SetTrigger("PlayAnimation");
+        }
+    }
+}
